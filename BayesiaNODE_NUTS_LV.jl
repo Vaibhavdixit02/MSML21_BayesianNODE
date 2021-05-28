ad51b9f7f26d5a92662bb274c26e5bcd892d19fa
Mohamed Tarek <mohamed82008@gmail.com>
2021-05-28 23:41:16 +1000

    update scripts

--- BayesiaNODE_NUTS_LV.jl
+++ BayesiaNODE_NUTS_LV.jl
@@ -13,17 +13,19 @@ u0 = [1.0, 1.0]
 # Simulation interval and intermediary points
 tspan = (0.0, 3.5)
 tsteps = 0.0:0.1:3.5
+datasize = length(tsteps)
 
 # LV equation parameter. p = [α, β, δ, γ]
 p = [1.5, 1.0, 3.0, 1.0]
 
 # Setup the ODE problem, then solve
 prob_ode = ODEProblem(lotka_volterra!, u0, tspan, p)
-sol_ode = solve(prob_ode, Tsit5(), saveat = tsteps)
-ode_data = hcat([sol_ode[:,i] for i in 1:size(sol_ode,2)]...)
+mean_ode_data = Array(solve(prob_ode, Tsit5(), saveat = tsteps))
+ode_data = mean_ode_data .+ 0.1 .* randn(size(mean_ode_data)..., 30)
 
-
-dudt2 = FastChain(FastDense(2, 20, tanh),
+dudt2 = FastChain(FastDense(2, 20, relu),
+                  FastDense(20, 20, relu),
+                  FastDense(20, 20, relu),
                   FastDense(20, 2))
 prob_neuralode = NeuralODE(dudt2, tspan, Tsit5(), saveat = tsteps)
 
@@ -37,55 +39,83 @@ function loss_neuralode(p)
     return loss, pred
 end
 
-l(θ) = -sum(abs2, ode_data .- predict_neuralode(θ)) - sum(θ .* θ)
+function l(θ)
+    lp = logpdf(MvNormal(zeros(length(θ) - 1), θ[end]), θ[1:end-1])
+    ll = -sum(abs2, ode_data .- predict_neuralode(θ[1:end-1]))
+    return lp + ll
+end
 function dldθ(θ)
-    x,lambda = Flux.Zygote.pullback(l,θ)
+    x, lambda = Flux.Zygote.pullback(l,θ)
     grad = first(lambda(1))
     return x, grad
 end
 
-metric  = DiagEuclideanMetric(length(prob_neuralode.p))
+init = [Float64.(prob_neuralode.p); 1.0]
+opt = DiffEqFlux.sciml_train(x -> -l(x), init, ADAM(0.05), maxiters = 1500)
+pmin = opt.minimizer;
+
+metric  = DiagEuclideanMetric(length(pmin))
 h = Hamiltonian(metric, l, dldθ)
-integrator = Leapfrog(find_good_stepsize(h, Float64.(prob_neuralode.p)))
-prop = NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator)
+integrator = Leapfrog(find_good_stepsize(h, pmin))
+prop = NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator, 5)
 
-adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.45, prop.integrator))
+adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.5, prop.integrator))
 
-samples, stats = sample(h, prop, Float64.(prob_neuralode.p), 500, adaptor, 2000; progress=true)
+samples, stats = sample(h, prop, pmin, 500, adaptor, 500; progress=true)
 
-
-losses = map(x-> x[1],[loss_neuralode(samples[i]) for i in 1:length(samples)])
+losses = map(x-> x[1],[loss_neuralode(samples[i][1:end-1]) for i in 1:length(samples)])
 
 ############################### PLOT LOSS ##################################
-scatter(losses, ylabel = "Loss",  label = "Architecture1: 2000 warmup, 500 sample")
+scatter(losses, ylabel = "Loss",  label = "Architecture1: 500 warmup, 500 sample")
 
-savefig("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/ExtendedLV_Loss_500_2000_Arch1.pdf")
+savefig("ExtendedLV_Loss_100_500_Arch1.pdf")
 
 ################################PLOT RETRODICTED DATA ##########################
-pl = scatter(tsteps, ode_data[1,:], color = :red, label = "Data: Var1", title = "Lotka Volterra Neural ODE")
-scatter!(tsteps, ode_data[2,:], color = :blue, label = "Data: Var2", xlabel = "t", ylims = (0, 10))
-
-for k in 1:300
-    resol = predict_neuralode(samples[100:end][rand(1:400)])
+pl = scatter(tsteps, mean_ode_data[1,:], color = :red, label = "Data: Var1", title = "Lotka Volterra Neural ODE")
+scatter!(tsteps, mean_ode_data[2,:], color = :blue, label = "Data: Var2", xlabel = "t", ylims = (0, 10))
+for _ in 1:300
+    resol = predict_neuralode(samples[100:end][rand(1:400)][1:end-1])
     plot!(tsteps, resol[1,:], alpha=0.04, color = :red, label = "")
     plot!(tsteps, resol[2,:], alpha=0.04, color = :blue, label = "")
 end
+resol = predict_neuralode(samples[100:end][rand(1:400)][1:end-1])
+plot!(tsteps, resol[1,:], alpha=0.04, color = :red, label = "")
+plot!(tsteps, resol[2,:], alpha=0.04, color = :blue, label = "")
 
 idx = findmin(losses)[2]
-prediction = predict_neuralode(samples[idx])
+prediction = predict_neuralode(samples[idx][1:end-1])
 
 plot!(tsteps, prediction[1,:], color = :black, w = 2, label = "")
 plot!(tsteps, prediction[2,:], color = :black, w = 2, label = "Best fit prediction")
 
 
 ################################CONTOUR PLOTS##########################
-pl = scatter(ode_data[1,:], ode_data[2,:], color = :red, label = "Data",  xlabel = "Var1", ylabel = "Var2", title = "Lotka Volterra Neural ODE")
-
-for k in 1:300
-    resol = predict_neuralode(samples[100:end][rand(1:400)])
-    plot!(resol[1,:],resol[2,:], alpha=0.04, color = :red, label = "")
+pl = scatter(
+    mean_ode_data[1,:],
+    mean_ode_data[2,:],
+    color = :blue, label = "Data",  xlabel = "Var1",
+    ylabel = "Var2", title = "Lotka Volterra Neural ODE",
+    legend = (0.85, 0.95), legendfontsize = 5,
+)
+for k in 1:size(ode_data, 3)
+    scatter!(
+        ode_data[1,:,k],
+        ode_data[2,:,k],
+        color = :blue, label = "",
+    )
 end
 
+for k1 in 301:500
+    σ = samples[k1][end]
+    resol = predict_neuralode(samples[k1][1:end-1])
+    for k2 in 1:10
+        _resol = resol .+ σ .* randn.()
+        label = ""
+        plot!(_resol[1,:], _resol[2,:], alpha=0.04, color = :red, label = label)
+    end
+end
+
+plot!(prediction[1,:], prediction[2,:], color = :red, w = 2, label = "Simulated data")
 plot!(prediction[1,:], prediction[2,:], color = :black, w = 2, label = "Best fit prediction")
 
 
@@ -102,7 +132,8 @@ u0_f = [1.0, 1.0]
 
 # Simulation interval and intermediary points
 tspan_f = (0.0, 4.5)
-tsteps_f = 0.0:0.1:4.5
+datasize_f = length(0.0:0.1:4.5)
+tsteps_f = [tsteps; range(tspan[2], tspan_f[2], length = datasize_f - datasize)]
 
 # LV equation parameter. p = [α, β, δ, γ]
 p = [1.5, 1.0, 3.0, 1.0]
@@ -112,54 +143,52 @@ prob_ode_f = ODEProblem(lotka_volterra!, u0_f, tspan_f, p)
 sol_ode_f = solve(prob_ode_f, Tsit5(), saveat = tsteps_f)
 ode_data_f = hcat([sol_ode_f[:,i] for i in 1:size(sol_ode_f,2)]...)
 
-dudt2_f = FastChain(FastDense(2, 20, tanh),
-                  FastDense(20, 2))
-prob_neuralode_f = NeuralODE(dudt2_f, tspan_f, Tsit5(), saveat = tsteps_f)
+prob_ode_f = ODEProblem(lotka_volterra!, u0_f, tspan_f, p)
+mean_ode_data_f = Array(solve(prob_ode_f, Tsit5(), saveat = tsteps_f))
+ode_data_f = cat(ode_data, mean_ode_data_f[:, datasize+1:end] .+ 0.1 .* randn(size(mean_ode_data_f, 1), size(mean_ode_data_f, 2) - datasize, 30), dims = 2)
+
+prob_neuralode_f = NeuralODE(dudt2, tspan_f, Tsit5(), saveat = tsteps_f)
 
 function predict_neuralode_f(p)
     Array(prob_neuralode_f(u0_f, p))
 end
 
-training_end = 3.5
-
-pl = scatter(tsteps_f, ode_data_f[1,:], color = :red, label = "Data: Var1", xlabel = "t", title = "Lotka Volterra Neural ODE")
-scatter!(tsteps_f, ode_data_f[2,:], color = :blue, label = "Data: Var2")
-
-for k in 1:300
-    resol = predict_neuralode_f(samples[100:end][rand(1:400)])
-    plot!(tsteps_f[1:36], resol[1,:][1:36], alpha=0.04, color = :red, label = "")
-    plot!(tsteps_f[1:36], resol[2,:][1:36], alpha=0.04, color = :blue, label = "")
-    plot!(tsteps_f[36:end], resol[1,:][36:end], alpha=0.04, color = :purple, label = "")
-    plot!(tsteps_f[36:end], resol[2,:][36:end], alpha=0.04, color = :purple, label = "")
-end
-
-
 idx = findmin(losses)[2]
 prediction_f = predict_neuralode_f(samples[idx])
 
-plot!(tsteps,prediction[1,:], color = :black, w = 2, label = "")
-plot!(tsteps,prediction[2,:], color = :black, w = 2, label = "Training: Best fit prediction")
+training_end = 3.5
 
-plot!([training_end-0.0001,training_end+0.0001],[-1,5],lw=3,color=:green,label="Training Data End", linestyle = :dash)
+pl = scatter(
+    mean_ode_data_f[1,:],
+    mean_ode_data_f[2,:],
+    color = :blue, label = "Data",  xlabel = "Var1",
+    ylabel = "Var2", title = "Lotka Volterra  Neural ODE",
+    legend = (0.8, 0.95), legendfontsize = 5,
+)
 
-plot!(tsteps_f[36:end],prediction_f[1,:][36:end], color = :purple, w = 2, label = "")
-plot!(tsteps_f[36:end],prediction_f[2,:][36:end], color = :purple, w = 2, label = "Forecasting: Best fit prediction", ylims = (-1.5, 10))
-
-
-savefig("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/ExtendedLV_Retrodicted_500_2000_Arch2.pdf")
-
-################################FORECASTING 2: CONTOUR PLOTS ##########################
-pl = scatter(ode_data_f[1,:], ode_data_f[2,:], color = :red, label = "Data",  xlabel = "Var1", ylabel = "Var2", title = "Lotka Volterra Neural ODE")
-
-for k in 1:300
-    resol = predict_neuralode_f(samples[100:end][rand(1:400)])
-    plot!(resol[1,:][1:36],resol[2,:][1:36], alpha=0.04, color = :red, label = "")
-    plot!(resol[1,:][36:end],resol[2,:][36:end], alpha=0.1, color = :purple, label = "")
+for k in 1:size(ode_data_f, 3)
+    scatter!(
+        ode_data_f[1,:,k],
+        ode_data_f[2,:,k],
+        color = :blue, label = "",
+    )
 end
 
-plot!(prediction[1,:], prediction[2,:], color = :black, w = 2, label = "Training: Best fit prediction")
-plot!(prediction_f[1,:][36:end], prediction_f[2,:][36:end], color = :purple, w = 2, label = "Forecasting: Best fit prediction", ylims = (-2, 7), xlims = (-0.5, 7))
+for k1 in 301:500
+    σ = samples[k1][end]
+    resol = predict_neuralode_f(samples[k1][1:end-1])
+    for k2 in 1:10
+        _resol = resol .+ σ .* randn.()
+        label = ""
+        plot!(_resol[1,:][1:datasize], _resol[2,:][1:datasize], alpha=0.02, color = :red, label = label)
+        plot!(_resol[1,:][datasize+1:end], _resol[2,:][datasize+1:end], alpha=0.04, color = :green, label = label)
+    end
+end
 
-savefig("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/ExtendedLV_Contour_Retrodicted_500_2000_Arch2.pdf")
+plot!(prediction_f[1,1:datasize], prediction_f[2,1:datasize], color = :red, w = 2, label = "Training: simulated data")
+plot!(prediction_f[1,datasize+1:end], prediction_f[2,datasize+1:end], color = :green, w = 2, label = "Forecasting: simulated data")
+plot!(prediction_f[1,1:datasize], prediction_f[2,1:datasize], color = :black, w = 2, label = "Best fit prediction")
 
-save("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/ExtendedLVNeuralODE_Architecture2_500_2000_0.45.jld", "losses",losses, "ode_data", ode_data, "samples", samples, "prediction", prediction, "stats", stats, "idx", idx, "tsteps", tsteps, "tsteps_f", tsteps_f )
+savefig("ExtendedLV_Contour_Retrodicted_500_500_Arch2.pdf")
+
+save("LVNeuralODE_Architecture1_500_500_0.5.jld", "losses", losses, "ode_data", ode_data, "mean_ode_data", mean_ode_data, "samples", samples, "prediction", prediction, "stats", stats, "idx", idx, "tsteps", tsteps)
--- BayesiaNODE_NUTS_Spiral.jl
+++ BayesiaNODE_NUTS_Spiral.jl
@@ -1,8 +1,8 @@
-cd("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE")
-Pkg.activate(".")
+#cd("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE")
+#Pkg.activate(".")
 
 using DiffEqFlux, OrdinaryDiffEq, Flux, Optim, Plots, AdvancedHMC
-using JLD, StatsPlots, Turing
+using JLD, StatsPlots
 
 u0 = [2.0; 0.0]
 datasize = 40
@@ -15,12 +15,12 @@ function trueODEfunc(du, u, p, t)
 end
 
 prob_trueode = ODEProblem(trueODEfunc, u0, tspan)
-ode_data = Array(solve(prob_trueode, Tsit5(), saveat = tsteps)) + 0.5 * randn(2,datasize)
-
+mean_ode_data = Array(solve(prob_trueode, Tsit5(), saveat = tsteps))
+ode_data = mean_ode_data .+ 0.1 .* randn(size(mean_ode_data)..., 30)
 
 ####DEFINE THE NEURAL ODE#####
 dudt2 = FastChain((x, p) -> x.^3,
-                  FastDense(2, 50, tanh),
+                  FastDense(2, 50, relu),
                   FastDense(50, 2))
 prob_neuralode = NeuralODE(dudt2, tspan, Tsit5(), saveat = tsteps)
 
@@ -34,36 +34,31 @@ function loss_neuralode(p)
     return loss, pred
 end
 
-l(θ) = -sum(abs2, ode_data .- predict_neuralode(θ)) - sum(θ .* θ)
-
-
-
+function l(θ)
+    lp = logpdf(MvNormal(zeros(length(θ) - 1), θ[end]), θ[1:end-1])
+    ll = -sum(abs2, ode_data .- predict_neuralode(θ[1:end-1]))
+    return lp + ll
+end
 function dldθ(θ)
-    x,lambda = Flux.Zygote.pullback(l,θ)
+    x, lambda = Flux.Zygote.pullback(l,θ)
     grad = first(lambda(1))
     return x, grad
 end
 
-metric  = DiagEuclideanMetric(length(prob_neuralode.p))
+init = [Float64.(prob_neuralode.p); 1.0]
+opt = DiffEqFlux.sciml_train(x -> -l(x), init, ADAM(0.05), maxiters = 1500)
+pmin = opt.minimizer;
 
+metric  = DiagEuclideanMetric(length(pmin))
 h = Hamiltonian(metric, l, dldθ)
+integrator = Leapfrog(find_good_stepsize(h, pmin))
+prop = AdvancedHMC.NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator, 5)
 
+adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.5, prop.integrator))
 
-#CHECK HOW MUCH TIME IT TAKES TO RUN THIS FIND_GOOD_STEP_SIZE FUNCTION###
-find_good_stepsize(h, Float64.(prob_neuralode.p))
+samples, stats = sample(h, prop, pmin, 500, adaptor, 500; progress=true)
 
-
-integrator = Leapfrog(find_good_stepsize(h, Float64.(prob_neuralode.p)))
-
-
-prop = AdvancedHMC.NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator)
-
-adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.45, prop.integrator))
-
-samples, stats = @time sample(h, prop, Float64.(prob_neuralode.p), 500, adaptor, 1000; progress=true)
-
-
-losses = map(x-> x[1],[loss_neuralode(samples[i]) for i in 1:length(samples)])
+losses = map(x-> x[1],[loss_neuralode(samples[i][1:end-1]) for i in 1:length(samples)])
 
 ############################### PLOTAND SAVE LOSS ##################################
 scatter(losses, ylabel = "Loss",  yscale= :log, label = "Architecture1: 1000 warmup, 500 sample")
@@ -71,45 +66,52 @@ savefig("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/Extendedtime_Spira
 
 
 ################################PLOT RETRODICTED DATA ##########################
-pl = scatter(tsteps, ode_data[1,:], color = :red, label = "Data: Var1", xlabel = "t", title = "Spiral Neural ODE")
-scatter!(tsteps, ode_data[2,:], color = :blue, label = "Data: Var2")
-
-for k in 1:300
-    resol = predict_neuralode(samples[100:end][rand(1:400)])
-    plot!(tsteps,resol[1,:], alpha=0.04, color = :red, label = "")
-    plot!(tsteps,resol[2,:], alpha=0.04, color = :blue, label = "")
+pl = scatter(tsteps, mean_ode_data[1,:], color = :red, label = "Data: Var1", title = "Spiral Neural ODE")
+scatter!(tsteps, mean_ode_data[2,:], color = :blue, label = "Data: Var2", xlabel = "t", ylims = (0, 10))
+for _ in 1:300
+    resol = predict_neuralode(samples[100:end][rand(1:400)][1:end-1])
+    plot!(tsteps, resol[1,:], alpha=0.04, color = :red, label = "")
+    plot!(tsteps, resol[2,:], alpha=0.04, color = :blue, label = "")
 end
+resol = predict_neuralode(samples[100:end][rand(1:400)][1:end-1])
+plot!(tsteps, resol[1,:], alpha=0.04, color = :red, label = "")
+plot!(tsteps, resol[2,:], alpha=0.04, color = :blue, label = "")
 
 idx = findmin(losses)[2]
-prediction = predict_neuralode(samples[idx])
+prediction = predict_neuralode(samples[idx][1:end-1])
 
-plot!(tsteps,prediction[1,:], color = :black, w = 2, label = "")
-plot!(tsteps,prediction[2,:], color = :black, w = 2, label = "Best fit prediction", ylims = (-2.5, 3.5))
+plot!(tsteps, prediction[1,:], color = :black, w = 2, label = "")
+plot!(tsteps, prediction[2,:], color = :black, w = 2, label = "Best fit prediction")
 
-
-
-model = @model function fit_node(data)
-    σ ~ InverseGamma(2, 3)
-    p ~ MvNormal(prob_neuralode.p, 1.0)
-    # Calculate predictions for the inputs given the params.
-    predicted = predict_neuralode(p)
-    # observe each prediction.
-
-    data ~ arraydist([MvNormal(predicted[:, i], σ) for i in 1:size(predicted, 2)])
-end
-
-chain = sample(model(ode_data), NUTS(0.65), 1000)
 
 ################################CONTOUR PLOTS##########################
-pl = scatter(ode_data[1,:], ode_data[2,:], color = :red, label = "Data",  xlabel = "Var1", ylabel = "Var2", title = "Spiral Neural ODE")
-
-for k in 1:300
-    resol = predict_neuralode(samples[100:end][rand(1:400)])
-    plot!(resol[1,:],resol[2,:], alpha=0.04, color = :red, label = "")
+pl = scatter(
+    mean_ode_data[1,:],
+    mean_ode_data[2,:],
+    color = :blue, label = "Data",  xlabel = "Var1",
+    ylabel = "Var2", title = "Spiral Neural ODE",
+    legend = (0.85, 0.95), legendfontsize = 5,
+)
+for k in 1:size(ode_data, 3)
+    scatter!(
+        ode_data[1,:,k],
+        ode_data[2,:,k],
+        color = :blue, label = "",
+    )
 end
 
-plot!(prediction[1,:], prediction[2,:], color = :black, w = 2, label = "Best fit prediction", ylims = (-2.5, 3))
+for k1 in 301:500
+    σ = samples[k1][end]
+    resol = predict_neuralode(samples[k1][1:end-1])
+    for k2 in 1:10
+        _resol = resol .+ σ .* randn.()
+        label = ""
+        plot!(_resol[1,:], _resol[2,:], alpha=0.02, color = :red, label = label)
+    end
+end
 
+plot!(prediction[1,:], prediction[2,:], color = :red, w = 2, label = "Simulated data")
+plot!(prediction[1,:], prediction[2,:], color = :black, w = 2, label = "Best fit prediction")
 
 
 ################################FORECASTING PLOTS########################
@@ -117,7 +119,7 @@ plot!(prediction[1,:], prediction[2,:], color = :black, w = 2, label = "Best fit
 u0 = [2.0; 0.0]
 datasize_f = 50
 tspan_f = (0.0, 1.2)
-tsteps_f = range(tspan_f[1], tspan_f[2], length = datasize_f)
+tsteps_f = [tsteps; range(tspan[2], tspan_f[2], length = datasize_f - datasize)]
 
 function trueODEfunc(du, u, p, t)
     true_A = [-0.1 2.0; -2.0 -0.1]
@@ -125,59 +127,50 @@ function trueODEfunc(du, u, p, t)
 end
 
 prob_trueode_f = ODEProblem(trueODEfunc, u0, tspan_f)
-ode_data_f = Array(solve(prob_trueode_f, Tsit5(), saveat = tsteps_f))
+mean_ode_data_f = Array(solve(prob_trueode_f, Tsit5(), saveat = tsteps_f))
+ode_data_f = cat(ode_data, mean_ode_data_f[:, datasize+1:end] .+ 0.1 .* randn(size(mean_ode_data_f, 1), size(mean_ode_data_f, 2) - datasize, 30), dims = 2)
 
-
-dudt2_f = FastChain((x, p) -> x.^3,
-                  FastDense(2, 50, tanh),
-                  FastDense(50, 2))
-prob_neuralode_f = NeuralODE(dudt2_f, tspan_f, Tsit5(), saveat = tsteps_f)
+prob_neuralode_f = NeuralODE(dudt2, tspan_f, Tsit5(), saveat = tsteps_f)
 
 function predict_neuralode_f(p)
     Array(prob_neuralode_f(u0, p))
 end
 
-################################FORECASTING 1: PLOT RETRODICTED DATA ##########################
-training_end = 1
-
-pl = scatter(tsteps_f, ode_data_f[1,:], color = :red, label = "Data: Var1", xlabel = "t", title = "Spiral Neural ODE")
-scatter!(tsteps_f, ode_data_f[2,:], color = :blue, label = "Data: Var2")
-
-for k in 1:300
-    resol = predict_neuralode_f(samples[100:end][rand(1:400)])
-    plot!(tsteps_f[1:42], resol[1,:][1:42], alpha=0.04, color = :red, label = "")
-    plot!(tsteps_f[1:42], resol[2,:][1:42], alpha=0.04, color = :blue, label = "")
-    plot!(tsteps_f[42:end], resol[1,:][42:end], alpha=0.04, color = :purple, label = "")
-    plot!(tsteps_f[42:end], resol[2,:][42:end], alpha=0.04, color = :purple, label = "")
-end
-
-
 idx = findmin(losses)[2]
 prediction_f = predict_neuralode_f(samples[idx])
 
-plot!(tsteps,prediction[1,:], color = :black, w = 2, label = "")
-plot!(tsteps,prediction[2,:], color = :black, w = 2, label = "Training: Best fit prediction", ylims = (-2.5, 3.5))
-plot!([training_end-0.0001,training_end+0.0001],[-2.2,1.3],lw=3,color=:green,label="Training Data End", linestyle = :dash)
+training_end = 1
+pl = scatter(
+    mean_ode_data_f[1,:],
+    mean_ode_data_f[2,:],
+    color = :blue, label = "Data",  xlabel = "Var1",
+    ylabel = "Var2", title = "Spiral Neural ODE",
+    legend = (0.8, 0.95), legendfontsize = 5,
+)
 
-plot!(tsteps_f[42:end],prediction_f[1,:][42:end], color = :purple, w = 2, label = "")
-plot!(tsteps_f[42:end],prediction_f[2,:][42:end], color = :purple, w = 2, label = "Forecasting: Best fit prediction", ylims = (-2.5, 3.5))
-
-savefig("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/ExtendedSpiralODE_Retrodicted_500_1000_Arch1.pdf")
-
-
-################################FORECASTING 2: CONTOUR PLOTS ##########################
-pl = scatter(ode_data_f[1,:], ode_data_f[2,:], color = :red, label = "Data",  xlabel = "Var1", ylabel = "Var2", title = "Spiral Neural ODE")
-
-for k in 1:300
-    resol = predict_neuralode_f(samples[100:end][rand(1:400)])
-    plot!(resol[1,:][1:42],resol[2,:][1:42], alpha=0.04, color = :red, label = "")
-    plot!(resol[1,:][42:end],resol[2,:][42:end], alpha=0.1, color = :purple, label = "")
-
+for k in 1:size(ode_data_f, 3)
+    scatter!(
+        ode_data_f[1,:,k],
+        ode_data_f[2,:,k],
+        color = :blue, label = "",
+    )
 end
 
-plot!(prediction[1,:], prediction[2,:], color = :black, w = 2, label = "Training: Best fit prediction", ylims = (-2.5, 3.5))
-plot!(prediction_f[1,:][41:end], prediction_f[2,:][41:end], color = :purple, w = 2, label = "Forecasting: Best fit prediction", ylims = (-2.5, 3.5))
+for k1 in 301:500
+    σ = samples[k1][end]
+    resol = predict_neuralode_f(samples[k1][1:end-1])
+    for k2 in 1:10
+        _resol = resol .+ σ .* randn.()
+        label = ""
+        plot!(_resol[1,:][1:datasize], _resol[2,:][1:datasize], alpha=0.02, color = :red, label = label)
+        plot!(_resol[1,:][datasize+1:end], _resol[2,:][datasize+1:end], alpha=0.02, color = :green, label = label)
+    end
+end
 
-savefig("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/ExtendedSpiralODE_Contour_Retrodicted_500_1000_Arch1.pdf")
+plot!(prediction_f[1,1:datasize], prediction_f[2,1:datasize], color = :red, w = 2, label = "Training: simulated data")
+plot!(prediction_f[1,datasize+1:end], prediction_f[2,datasize+1:end], color = :green, w = 2, label = "Forecasting: simulated data")
+plot!(prediction_f[1,:], prediction_f[2,:], color = :black, w = 2, label = "Best fit prediction")
 
-save("C:/Users/16174/Desktop/Julia Lab/Bayesian Neural ODE/ExtendedSpiralNeuralODE_Architecture1_500_1000_0.45.jld", "losses",losses, "ode_data", ode_data, "samples", samples, "prediction", prediction, "stats", stats, "idx", idx, "tsteps", tsteps, "tsteps_f", tsteps_f )
+savefig("ExtendedSpiralODE_Contour_Retrodicted_500_500_Arch1.pdf")
+
+save("ExtendedSpiralNeuralODE_Architecture1_500_500_0.50.jld", "losses",losses, "ode_data", ode_data, "samples", samples, "prediction", prediction, "stats", stats, "idx", idx, "tsteps", tsteps, "tsteps_f", tsteps_f )
